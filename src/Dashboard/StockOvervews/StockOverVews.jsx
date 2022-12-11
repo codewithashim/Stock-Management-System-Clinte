f405321284400e4f3680f73b7fd3330508877479
codewithashim <codewithashim@gmail.com>
2022-12-11 21:00:27 +0600

    adde overvvew maneg stock

--- src/Dashboard/StockOvervews/StockOverVews.jsx
+++ src/Dashboard/StockOvervews/StockOverVews.jsx
@@ -24,7 +24,8 @@ const StockOverVews = () => {
         <h1>All Stock Overview</h1>
         <div className="divider"></div>
       </div>
-      <div className="flex justify-center items-center gap-6">
+      <div className="my-4 font-bold text-2xl">Add New Stock</div>
+      <div className="grid md:grid-cols-3 justify-center items-center gap-6">
         {/* =========1======== */}
         <div>
           <div className="grid p-4 h-20 rounded bg-primary text-primary-content place-content-center">
@@ -70,6 +71,49 @@ const StockOverVews = () => {
           </div>
         </div>
       </div>
+      <div>
+        <div className=" my-4 font-bold text-2xl">Maneg Stock</div>
+      </div>
+
+      <div className="grid md:grid-cols-3 gap-4 ">
+        <div>
+          <div className="grid p-4 h-20 rounded bg-primary text-primary-content place-content-center">
+            <div className="text-center">
+              <h2 className="text-2xl font-bold text-white mb-2">
+                Lend Stocks
+              </h2>
+              <p>Total : {allReturnedStocks?.length}</p>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        {/* ========5======== */}
+        <div>
+          <div className="grid p-4 h-20 rounded bg-primary text-primary-content place-content-center">
+            <div className="text-center">
+              <h2 className="text-2xl font-bold text-white mb-2">
+                Return Stocks
+              </h2>
+              <p>Total : {allReturnedStocks?.length}</p>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        {/* ========6======== */}
+        <div>
+          <div className="grid p-4 h-20 rounded bg-primary text-primary-content place-content-center">
+            <div className="text-center">
+              <h2 className="text-2xl font-bold text-white mb-2">
+                Sold Stocks
+              </h2>
+              <p>Total : {allReturnedStocks?.length}</p>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider"></div>
     </section>
   );
 };
